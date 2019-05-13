6acd3d13753c5d72833863ef754f5fa658dff5e9
zackdk <zackvvzackvv@gmail.com>
2019-05-13 23:02:20 +0800

    nothing

--- js/index2.js
+++ js/index2.js
@@ -1,21 +1,7 @@
-addListener(window, 'resize', function () {
-    $(".nav ul").removeAttr("style")
-});
+function init(){
+    
+}
 
 
-$(".nav img").click(function(){
-    // $(".nav ul").css("opacity","1"); 
-    // $(".nav ul").css("height","auto");
-    $(".nav ul").slideToggle();   
-});
 
-var swiper = new Swiper('.swiper-container', {
-    pagination: {
-      el: '.swiper-pagination',
-    },
-});
-
-let canvas = document.getElementById('canvas');
-canvas.width = canvas.clientWidth;
-canvas.height = canvas.clientHeight;
-firework.start(document.getElementById("canvas"),10)
+init();
