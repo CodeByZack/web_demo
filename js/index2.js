aa7a4653e0db08da8b279d9411c2de1cf6773e7c
zackdk <958059970@qq.com>
2019-05-15 22:34:37 +0800

    根据当前时间 算工作时间 和 年龄

--- js/index2.js
+++ js/index2.js
@@ -1,5 +1,19 @@
 function init(){
-    
+    const birth = dayjs("1995-01-10");
+    const worktime = dayjs("2017-06-01");
+
+    let now = dayjs();
+
+    let old = now.diff(birth,'year'); 
+    let yearsOfWorking = now.diff(worktime,'day');
+
+    console.log(old,yearsOfWorking);
+
+    let ageDom = document.querySelector(".age");
+    let workDom = document.querySelector(".work");
+
+    ageDom.innerText = `年龄：${ old }`;
+    workDom.innerText = `工作经验：${ yearsOfWorking } 天`;
 }
 
 
