eb776ba76b43518c0e90bcd8d86151d2f5321500
董凯 <dongk@travelsky.com>
2020-03-11 21:26:43 +0800

    update

--- README.md
+++ README.md
@@ -7,4 +7,32 @@
 
 ## 无聊的练习
 
-1. [帧动画](demo/帧动画.html)
+1、[base64](demo/base64)
+
+2、[css等高布局](demo/css等高布局)
+
+3、[demo11](demo/demo11)
+
+4、[webcomponents](demo/webcomponents)
+
+5、[中国地图](demo/中国地图)
+
+6、[前端history路由](demo/前端history路由)
+
+7、[垂直水平居中](demo/垂直水平居中)
+
+8、[字体高度测试](demo/字体高度测试)
+
+9、[小球滚动](demo/小球滚动)
+
+10、[帧动画](demo/帧动画)
+
+11、[弹幕模拟](demo/弹幕模拟)
+
+12、[烟花效果](demo/烟花效果)
+
+13、[粒子文字](demo/粒子文字)
+
+14、[雨滴特效](demo/雨滴特效)
+
+15、[鼠标跟随粒子效果](demo/鼠标跟随粒子效果)
---
+++ node/updateReadme.js
@@ -0,0 +1,45 @@
+const fs = require('fs');
+const path = require('path');
+const isWin = /^win/.test(process.platform);
+const notMined = ['.DS_Store', '.vscode', '.idea']
+const sysFolderFilter = file => !notMined.includes(file) //过滤器,过滤掉系统文件
+
+//demo路径
+const demoDirPath = path.resolve('demo');
+const templatePath = path.resolve(__dirname,'template.md');
+const targetReadme = path.resolve('README.md');
+
+const allPaths = fs.readdirSync(demoDirPath);
+const allHtmls = allPaths.filter(p=>path.parse(p).ext === '.html');
+
+const htmlLinks = allHtmls.map((l,i)=>{
+    const name = path.parse(l).name;
+    let mdLink =  `${i+1}、[${name}](${path.join('demo',name)})`;
+    if(isWin){
+        mdLink = mdLink.replace(/\\/g, "/");
+    }
+    return mdLink;
+}).join('\n\n');
+
+console.log(htmlLinks)
+
+
+const templateContent = fs.readFileSync(templatePath, 'utf8');
+const resContent = templateContent.replace('{{demo-liniks}}', htmlLinks);
+fs.writeFileSync(targetReadme, resContent, 'utf8');
+
+console.log('目录生成成功~')
+// //demo
+// let articles = fs.readdirSync(article).filter(sysFolderFilter).map((p, i) => {
+//   let art = path.relative(path.resolve('.'), p)
+//   return `${i+1}、[${art}](${path.join('article',art)})`
+// }).join('\n\n')
+
+// //目录模板
+// let template = path.resolve(__dirname, 'template.md')
+// let content = fs.readFileSync(template, 'utf8')
+// //处理结果
+// let resContent = content.replace('{{article}}', articles)
+// //写入Readme
+// let readme = path.resolve('README.md')
+// fs.writeFileSync(readme, resContent, 'utf8')
