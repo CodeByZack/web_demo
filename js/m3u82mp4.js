c428f39988a35286280d151d4b3cb7ae370e6619
zackdk <958059970@qq.com>
2020-11-23 17:21:50 +0800

    fix

--- js/m3u82mp4.js
+++ js/m3u82mp4.js
@@ -58,18 +58,25 @@ const praseM3u8 = async (url)=>{
 const merge2mp4 = async (url,logger = console.log)=>{
   const { createFFmpeg, fetchFile } = FFmpeg;
   const ffmpeg = createFFmpeg();
+
+  logger(`开始下载${url},并获取所有ts片段链接`);
   const praseObj = await praseM3u8(url);
+
   // 如果有多个码率,默认选第一个
   const [link,tsArr] = Object.entries(praseObj)[0];
+  
   await ffmpeg.load();
   ffmpeg.FS('writeFile', 'index.m3u8', await fetchFile(link));
   ffmpeg.setLogging(false);
-  ffmpeg.setProgress(console.log);
+  ffmpeg.setProgress(({ratio})=>{
+    logger(`合并进度${ratio}.`);
+  });
   for (const ts of tsArr) {
       ffmpeg.FS('writeFile', ts.name, await fetchFile(ts.path));
       const index = tsArr.indexOf(ts);
       logger(`正在下载ts片段: 第${index}段，共${tsArr.length}段`);
   }
+  logger(`开始执行合并.`)
   await ffmpeg.run('-i', 'index.m3u8', '-c', 'copy' , 'output.mp4');
   const data = ffmpeg.FS('readFile', 'output.mp4');
 
